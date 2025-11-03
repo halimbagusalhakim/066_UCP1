76d16f0e0aa6e2fb07d6d9790d311495ab4c0572
halimbagusalhakim <halsat132@gmail.com>
2025-11-03 08:32:07 +0700

    setup db sequelize untuk di index js

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ const db = require('./models');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT), () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}
+
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    })
+}).catch((err) => {
+    console.log(err);
+});
